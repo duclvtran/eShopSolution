a43c78cda9692160fadb1fee9b73a9dce7af0338
Duc Linh Vu Tran <linhvu.vn@gmail.com>
2020-04-27 22:34:20 -0700

    AddPublicProductService_ManageProductService

---
+++ eShopSolution.Data/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+    {
+        public void Configure(EntityTypeBuilder<ProductImage> builder)
+        {
+            builder.ToTable("ProductImages");
+
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Product).WithMany(x => x.ProductImages).HasForeignKey(x => x.ProductId);
+        }
+    }
+}
---
+++ eShopSolution.Utilities/Exceptions/EShopException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace eShopSolution.Utilities.Exceptions
+{
+    public class EShopException : Exception
+    {
+        public EShopException()
+        {
+        }
+
+        public EShopException(string message) : base(message)
+        {
+        }
+
+        public EShopException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected EShopException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
